1ea0a977671b9dd1fbba90fae40b846b804e76ae
github username <github email address>
2019-07-25 13:30:34 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,8 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+    def owner_count
+        self.employees.count
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,14 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :first_name, length: {minimum: 2}, presence: true
+    validates :last_name, length: {minimum: 2}, presence: true
+    validates :alias, uniqueness: true, presence: true
+    validates :title, uniqueness: true, presence: true
+    validates :title, presence: true
+    validates :dog_id, length: {is: 1}
+    validates :img_url, presence: true
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
